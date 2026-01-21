fecea8e38f140aa14d46852deb60f5126f180925
beer-psi <lacvtg.a1.2023@gmail.com>
2026-01-21 14:01:33 +0700

    feat(wc): add options for individual counts

--- user/wc.c
+++ user/wc.c
@@ -5,50 +5,94 @@
 
 char buf[512];
 
+enum {
+  WC_LINES = 0,
+  WC_WORDS = 1,
+  WC_BYTES = 2,
+  NUM_WCS = 3,
+};
+
 void
-wc(int fd, char *name)
+wc(int fd, char *name, unsigned print_type)
 {
   int i, n;
-  int l, w, c, inword;
+  int inword;
+  int counts[NUM_WCS];
+  char* fmt = "%d";
 
-  l = w = c = 0;
   inword = 0;
-  while((n = read(fd, buf, sizeof(buf))) > 0){
+  while((n = read(fd, buf, sizeof(buf))) > 0)
     for(i=0; i<n; i++){
-      c++;
+      counts[WC_BYTES]++;
       if(buf[i] == '\n')
-        l++;
+        counts[WC_LINES]++;
       if(strchr(" \r\t\n\v", buf[i]))
         inword = 0;
       else if(!inword){
-        w++;
+        counts[WC_WORDS]++;
         inword = 1;
       }
     }
-  }
   if(n < 0){
     printf("wc: read error\n");
     exit(1);
   }
-  printf("%d %d %d %s\n", l, w, c, name);
+
+  for(i = 0; i < NUM_WCS; i++)
+    if(print_type & (1 << i)){
+      printf(fmt, counts[i]);
+      fmt = " %d";
+    }
+  printf(" %s\n", name);
 }
 
 int
 main(int argc, char *argv[])
 {
   int fd, i;
+  unsigned print_type = 0, optind = 1;
 
-  if(argc <= 1){
-    wc(0, "");
+  // Argument parsing.
+  // TODO: Handle options being intermixed with arguments by moving
+  // options to the front of argv.
+  for(optind = 1; optind < argc; optind++){
+    if(argv[optind][0] != '-' || argv[optind][1] == '\0')break;
+
+    switch(argv[optind][1]){
+    case 'l':
+      print_type |= 1 << WC_LINES;
+      break;
+    case 'w':
+      print_type |= 1 << WC_WORDS;
+      break;
+    case 'c':
+      print_type |= 1 << WC_BYTES;
+      break;
+    default:
+      printf("wc: invalid option %s", argv[optind]);
+      exit(22);   // EINVAL
+    }
+  }
+
+  // Decrease arg count by the number of options we've parsed,
+  // and increase the arg pointer by the same amount.
+  argc -= optind;
+  argv += optind;
+
+  if(print_type == 0)
+    print_type = (1 << WC_LINES) | (1 << WC_WORDS) | (1 << WC_BYTES);
+
+  if(argc <= 0){
+    wc(0, "", print_type);
     exit(0);
   }
 
-  for(i = 1; i < argc; i++){
+  for(i = 0; i < argc; i++){
     if((fd = open(argv[i], O_RDONLY)) < 0){
       printf("wc: cannot open %s\n", argv[i]);
       exit(1);
     }
-    wc(fd, argv[i]);
+    wc(fd, argv[i], print_type);
     close(fd);
   }
   exit(0);
